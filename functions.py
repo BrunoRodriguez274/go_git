99ffd1acc14d29c01ae3b2a150543c314f170042
DS-CARLOS-RODRIGUEZ <carlos.rodriguez@DEV-CARLOS-RODRIGUEZ.local>
2024-06-19 03:30:32 -0600

    Nueva función de producto

--- functions.py
+++ functions.py
@@ -2,14 +2,26 @@
 def suma_normal(a,b):
 	return a+b
 
-
 def producto_normal(a,b):
 	return a*b
 
-def suma(*args):
+def suma_general(*args):
 
 	total = 0
 	for i in args:
 		total +=i
 
+	print(total)
 	return total
+
+
+def producto_general(*args):
+	
+	total = 0
+	for i in args:
+		total +=i
+
+	print(total)
+
+	return total
+
